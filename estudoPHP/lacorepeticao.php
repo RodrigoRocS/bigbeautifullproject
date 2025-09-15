9e898aae61d36d6ae863b854cd03c476f8c92476
Rodrigo Rocha <rrsilva1701@gmail.com>
2025-09-15 18:32:14 -0300

    teste laço repetição php

---
+++ estudoPHP/lacorepeticao.php
@@ -0,0 +1,28 @@
+<?php
+
+$i = 2;
+while ($i <= 20) {
+    echo $i;
+    $i += 2;
+    echo "\n";
+}
+
+/*Sintaxe alternativa*/
+// $i = 2;
+// while ($i <= 20):
+//     echo $i;
+//     $i += 2;
+//     echo "\n";
+// endwhile;
+
+$f = 2;
+do {
+    echo "F$f";
+    $f += 2;
+    echo "\n";
+} while ($f <= 20);
+
+for ($a = 1; $a <= 20; $a++) {
+    echo "A$a";
+    echo "\n";
+}
